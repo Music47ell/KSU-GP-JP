97578b3563245cfff49ed55bbc79c33546ff39a1
Ahmet ALMAZ <5708563+Music47ell@users.noreply.github.com>
2026-07-21 01:03:30 +0300

    fix: correct domain from ksu-gp-jps to ksu-gp-jp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ksu-gp-jps.ahmetalmaz.com",
+	site: "https://ksu-gp-jp.ahmetalmaz.com",
 	base: "/",
 	trailingSlash: "never",
 	integrations: [tailwind()],
